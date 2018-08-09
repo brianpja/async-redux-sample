0ba7015994e56b5dfc020ca1685d2ffb815e0c60
Brian James <brian.james@slalom.com>
2018-08-09 12:03:04 -0700

    reducers and actions written, thunk middleware implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPostsIfNeeded } from './actions';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+);
+
+store.dispatch(selectSubreddit('reactjs'))
+store
+    .dispatch(fetchPostsIfNeeded('reactjs'))
+    .then(() => console.log(store.getState()));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
